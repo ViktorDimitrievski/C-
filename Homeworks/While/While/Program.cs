bf8070b7adaa7bbe88fcc6b3211e0d34b8d2cc6c
ednodete <viktor_dimitrievski@hotmail.com>
2016-02-20 11:57:12 +0100

    full add new things

---
+++ Homeworks/While/While/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //whilePrintNumbersAscending();
+            //whilePrintNumbersDescending();
+            //whilePrintEvenNumbers();
+            // whilePrintOddNumbers();
+            whileBreakForLoop();
+            Console.ReadLine();
+
+        }
+
+        /*public static void whilePrintNumbersAscending()
+        {
+            int inputNumStart, inputNumEnd;
+
+            Console.WriteLine("Enter Start number = ");
+            inputNumStart = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ENDs number = ");
+            inputNumEnd = int.Parse(Console.ReadLine());
+
+            while (inputNumStart <= inputNumEnd)
+            {
+                Console.WriteLine(inputNumStart + "\t");
+                inputNumStart++;
+
+            }
+        }*/
+
+        /*public static void whilePrintNumbersDescending()
+        {
+            int inputNumStart, inputNumEnd;
+
+            Console.WriteLine("Enter Start number = ");
+            inputNumStart = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ENDs number = ");
+            inputNumEnd = int.Parse(Console.ReadLine());
+
+            while (inputNumEnd >= inputNumStart)
+            {
+                Console.WriteLine(inputNumEnd + "\t");
+                inputNumEnd--;
+
+            }
+        }*/
+
+        /* public static void whilePrintEvenNumbers()
+         {
+             int inputNumStart = 2, inputNumEnd;
+
+
+             Console.WriteLine("Enter ENDs number = ");
+             inputNumEnd = int.Parse(Console.ReadLine());
+
+             while (inputNumStart <= inputNumEnd)
+             {
+                 Console.Write(inputNumStart + "\t");
+                 inputNumStart += 2;
+
+             }
+         }*/
+
+
+        /* public static void whilePrintOddNumbers()
+         {
+             int inputNumStart = 1, inputNumEnd;
+
+
+             Console.WriteLine("Enter ENDs number = ");
+             inputNumEnd = int.Parse(Console.ReadLine());
+
+             while (inputNumStart <= inputNumEnd)
+             {
+                 Console.Write(inputNumStart + "\t");
+                 inputNumStart += 2;
+
+             }
+         }*/
+
+        public static void whileBreakForLoop()
+        {
+            int inputNumStart = 0;
+
+            while (inputNumStart < 10)
+            {
+                if (inputNumStart == 7)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write(inputNumStart + "\t");
+                    inputNumStart++;
+                }
+            }
+        }
+
+    }
+}
---
+++ Homeworks/task-1/task-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my first console app from VS 2015 at my home on 16.02.2015 - 13: 39");
+            Console.WriteLine("Hello SEDC Hello VS 2015 Hello C#");
+            Console.ReadLine();
+        }
+    }
+}
